5ae3723df02424bdda0fbbc89d039f7032bad8c8
DevPatils <dev.patil1006@gmail.com>
2024-06-25 20:10:46 +0530

    modified:   src/components/Register.tsx

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <>
             <h1 className='text-2xl font-bold my-3 text-center'>Register</h1>
             <div className='flex flex-col w-1/2 mx-auto border-2 rounded-lg bg-gray-800 p-4'>
                 <div className="flex items-center mb-4">
                     <label className="w-1/4 mr-4">Name</label>
-                    <input type="text" className="grow input input-bordered" placeholder="Daisy" />
+                    <input type="text" className="grow input input-bordered" placeholder="Daisy" value={name} onChange={(e) => setName(e.target.value)} />
                 </div>
                 <div className="flex items-center mb-4">
                     <label className="w-1/4 mr-4">Email</label>
-                    <input type="text" className="grow input input-bordered" placeholder="daisy@site.com" />
+                    <input type="text" className="grow input input-bordered" placeholder="daisy@site.com" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="flex items-center mb-4">
                     <label className="w-1/4 mr-4">Phone</label>
-                    <input type="text" className="grow input input-bordered" placeholder="123-456-7890" />
+                    <input type="text" className="grow input input-bordered" placeholder="123-456-7890" value={phone} onChange={(e) => setPhone(e.target.value)} />
                 </div>
                 <div className="flex items-center mb-4">
                     <label className="w-1/4 mr-4">Password</label>
-                    <input type="password" className="grow input input-bordered" placeholder="********" />
+                    <input type="password" className="grow input input-bordered" placeholder="********" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div className='flex justify-center items-center gap-4 flex-col'>
-                    <button className="btn btn-outline btn-warning">Submit</button>
+                    <button type='submit' className="btn btn-outline btn-warning">Submit</button>
                     <p>Already an Account? Login</p>
                 </div>
 
