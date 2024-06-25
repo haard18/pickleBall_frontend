76d6297b309b73cd69d13c9ab238983be802359a
DevPatils <dev.patil1006@gmail.com>
2024-06-25 17:34:45 +0530

    Some updates to the landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,34 +2,12 @@ import React from 'react';
 import logo from "../assets/images/logo.png"
 export const Navbar = () => {
     return (
-        // <>
-        //   <div className="flex flex-row bg-[#223649]">
-        //     <div className="flex flex-row gap-[1vw]">
-        //       <div className="flex items-center">
-        //         <img src={logo} alt="" className="h-[4vw] w-[4vw]" />
-        //       </div>
-        //       <h3 className="mt-[1vw] text-white">USA</h3>
-        //     </div>
-        //     <div className="flex flex-row gap-[1vw] ml-auto mr-[75vw]">
-        //       <div className="flex flex-row gap-[3vw] mt-[1.5vw]">
-        //         <div className="flex flex-row gap-[1vw] items-center">
-        //           <i className="fa-solid fa-phone text-[#336A25]" id='call-icon'></i>
-        //           <button className="bg-[#223649] text-white font-bold text-[1vw] mt-[-0.5vw] border-none cursor-pointer">Contact Us</button>
-        //         </div>
-        //         <div className="flex flex-row gap-[1vw] items-center">
-        //           <i className="fa-regular fa-user text-white" id='user-icon'></i>
-        //           <button className="bg-[#223649] text-white font-bold text-[1vw] mt-[-0.5vw] border-none cursor-pointer">Login</button>
-        //         </div>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </>
         <>
-            <div className="flex justify-between mt-4 items-center">
+            <div className="flex justify-between mt-4 items-center ">
                 <div className='flex items-center'>
 
                     <div className='flex mx-4'>
-                        <img src={logo} alt="" className='w-10 h-10' />
+                        <img src={logo} alt="" className='w-14 h-14' />
                     </div>
                     <div className='text-lg font-semibold '>
                         USA SPORTS
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Card from '../components/Card'
+import hero from "../assets/images/WhatsApp_Image_2024-06-25_at_15.57.07_42191b53-removebg-preview.png"
 
 const Home = () => {
     return (
-        <div>
+        <>
+        <div className="home  bg-gradient-to-r from-custom-gold to-custom-black">
             <Navbar />
-            <div className='flex-col'>
-                <Card image='https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b' title='Turf 1' location='Location 1' />
+
+            <div className=" home grid gap-20 grid-cols-2 ml-20 mt-20 ">
+                <div className="left mt-20" >
+                    <h1 className='text-7xl'>United sports<br />Arena</h1>
+                    <p className='text-2xl mt-1 italic'>“Your Ultimate Court Destination!"</p>
+                    <button className="btn px-4 py-2 font-serif mt-4">Get Started</button>
+                </div>
+                <div className="right ml-12">
+                    <img src={hero} alt="Hero image" />
+                </div>
             </div>
+            <div className=" flex-col justify-center  mt-10"style={{marginLeft:"30vw"}}>
+                <h1 className='text-5xl'>Book Courts and Turfs </h1>
+                <h1 className='text-1xl italic mt-4'>"Playing turf soccer with friends at United Sports Arena means excitement, teamwork, and<br/> unforgettable moments on the perfect playing surface".</h1>
+            </div>
+
         </div>
+        </>
     )
 }
 
