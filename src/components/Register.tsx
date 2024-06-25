ac3b500f60d74feb7955a35ee6420afffb10fa24
haard18 <haardsolanki.itm@gmail.com>
2024-06-25 19:50:20 +0530

    refactor: Update Register component UI and add submit button

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -4,23 +4,28 @@ const Register = () => {
     return (
         <>
             <h1 className='text-2xl font-bold my-3 text-center'>Register</h1>
-            <div className='flex flex-col w-1/2 mx-auto border-2 p-4'>
+            <div className='flex flex-col w-1/2 mx-auto border-2 rounded-lg bg-gray-800 p-4'>
                 <div className="flex items-center mb-4">
-                    <label className="w-1/4 text-right mr-4">Name</label>
+                    <label className="w-1/4 mr-4">Name</label>
                     <input type="text" className="grow input input-bordered" placeholder="Daisy" />
                 </div>
                 <div className="flex items-center mb-4">
-                    <label className="w-1/4 text-right mr-4">Email</label>
+                    <label className="w-1/4 mr-4">Email</label>
                     <input type="text" className="grow input input-bordered" placeholder="daisy@site.com" />
                 </div>
                 <div className="flex items-center mb-4">
-                    <label className="w-1/4 text-right mr-4">Phone</label>
+                    <label className="w-1/4 mr-4">Phone</label>
                     <input type="text" className="grow input input-bordered" placeholder="123-456-7890" />
                 </div>
                 <div className="flex items-center mb-4">
-                    <label className="w-1/4 text-right mr-4">Password</label>
+                    <label className="w-1/4 mr-4">Password</label>
                     <input type="password" className="grow input input-bordered" placeholder="********" />
                 </div>
+                <div className='flex justify-center items-center gap-4 flex-col'>
+                    <button className="btn btn-outline btn-warning">Submit</button>
+                    <p>Already an Account? Login</p>
+                </div>
+
             </div>
 
         </>
