f68e886f91b9e504b6e90fbc93e4ea9c5d9fcf1a
DevPatils <dev.patil1006@gmail.com>
2024-06-26 22:54:46 +0530

    changes on booking page

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,57 +1,17 @@
 import React from 'react';
-import Carousal from '../components/Carousal';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt, faShareAlt, faStar } from '@fortawesome/free-solid-svg-icons';
-import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
+
 import Navbar from '../components/Navbar';
+import { TurffDetails } from '../components/TurffDetails';
 
 export const BookingPage = () => {
     return (
         <>
 
             <Navbar />
-            <div className='flex w-full justify-center '>
-                <div className="BookingsPage rounded-lg flex font-serif justify-between mt-10 bg-gray-800 p-4 w-4/5 items-center">
-
-                    <div className="flex flex-col justify-center items-center h-[200px] w-1/4" >
-                        <Carousal />
-                    </div>
-
-                    <div className="flex gap-4 justify-center items-center">
-                        <div className="address gap-5 flex flex-col items-center" >
-                            <h3 className="text-4xl">Pickle Ball Court</h3>
-                            <div className='flex'>
-                                <div className="location-marker">
-                                    <FontAwesomeIcon icon={faMapMarkerAlt} />
-                                </div>
-                                <p className="text-1xl w-80 text-center">
-                                    Behind Doggers Park, Opposite Veda Lawns, Near Greenwood Antica, Ankodiya Sevasi Road Vadodara, Gujarat 391330
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center gap-5 ">
-
-
-                        <div className="flex flex-row gap-5 ">
-                            <FontAwesomeIcon icon={farHeart} />
-                            <FontAwesomeIcon icon={faShareAlt} />
-                        </div>
-
-                        <div className="ratings flex flex-row gap-2  p-1 w-16">
-                            <h1>5.00</h1>
-                            <FontAwesomeIcon icon={faStar} className='mt-1' />
-
-                        </div>
-
-                        <div className="flex flex-col">
-
-                            <button className="btn btn-outline">
-                                Get Directions
-                            </button>
-                        </div>
-                    </div>
-                </div>
+            <TurffDetails/>
+            <div className="page-nav flex justify-center gap-4  bg-gray-800 w-4/5">
+                <h1>Book Slots</h1>
+                <h1>Details</h1>
             </div>
 
 
