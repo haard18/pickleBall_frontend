6dbc3de30b66bde4c11cc1d7b95009fde152b8b9
haard18 <haardsolanki.itm@gmail.com>
2024-06-28 14:39:34 +0530

    refactor: Update BookingCard and Choose components

--- src/components/BookingCard.tsx
+++ src/components/BookingCard.tsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import img from "../assets/images/pckl.jpg"
-const BookingCard = ({title,facility,rate}:{title:string,facility:string,rate:string}) => {
+type BookingCardProps = {
+    title: string;
+    facility: string;
+    rate: string;
+    onclick: () => void;
+};
+
+const BookingCard = ({ title, facility, rate, onclick }: BookingCardProps) => {
     return (
         <div>
             <div className="card bg-base-100 w-96 shadow-xl">
                 <figure className="pt-10">
-                    <img
-                        src={img}
-                        alt={title}
-                        className="rounded-xl" />
+                    <img src={img} alt={title} className="rounded-xl" />
                 </figure>
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">{title}</h2>
                     <p>{facility}</p>
                     <div className="card-actions flex items-center">
-                        <p>{"₹"+rate+""}</p>
-                        <button className="btn btn-neutral">Book slot</button>
+                        <p>{"₹" + rate + ""}</p>
+                        <button onClick={onclick} className="btn btn-neutral">
+                            Book slot
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default BookingCard
--- src/components/Choose.tsx
+++ src/components/Choose.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BookingCard from './BookingCard'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fa1 } from '@fortawesome/free-solid-svg-icons'
+import { fa1, fa2, fa3 } from '@fortawesome/free-solid-svg-icons'
 export const Choose = () => {
+  const [card1, setcard1] = useState(true);
+  const [card2, setcard2] = useState(false);
+  const [card3, setcard3] = useState(false);
+  const handleClick1 = () => {
+    setcard1(false);
+    setcard2(true);
+  }
+  const handleClick2 = () => {
+    setcard2(false);
+    setcard3(true);
+  }
   return (
     <>
       <div className="choose">
@@ -12,68 +23,50 @@ export const Choose = () => {
               <div className="timeline-middle">
                 <FontAwesomeIcon icon={fa1} />
               </div>
-              <div className="timeline-end timeline-box"><BookingCard title={"PickeBall Turf"} rate={"350"} facility={"Indoor turf"} /></div>
-              <hr />
-            </li>
-            <li>
-              <hr />
-              <div className="timeline-middle">
-                <FontAwesomeIcon icon={fa1} />
+              <div className="timeline-end timeline-box">
+                {card1 ?
 
+                  <BookingCard onclick={handleClick1} title={"PickeBall Turf"} rate={"350"} facility={"Indoor turf"} />
+
+                  : <div className="timeline-end timeline-box flex items-center justify-between w-[40vw]">
+
+                    <p className='text-2xl '> Choose a Facility</p>
+                   {card2&& <button onClick={() => {
+                      setcard1(true);
+                      setcard2(false);
+                    }} className='btn btn-neutral '>Change</button>}
+                  </div>}
               </div>
-              <div className="timeline-end timeline-box">iMac</div>
               <hr />
             </li>
             <li>
               <hr />
               <div className="timeline-middle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd" />
-                </svg>
+                <FontAwesomeIcon icon={fa2} />
               </div>
-              <div className="timeline-end timeline-box">iPod</div>
+              <div className="timeline-end timeline-box">
+                {card2 ? <BookingCard onclick={handleClick2} title={"Choose a facility"} rate={"350"} facility={"Indoor turf"} /> : <div className="timeline-end timeline-box flex items-center justify-between w-[40vw]">
+
+                  <p className='text-2xl '> Choose a Facility</p>
+                  <button onClick={() => {
+                    setcard2(true);
+                    setcard3(false);
+                  }} className='btn btn-neutral '>Change</button>
+                </div>}
+              </div>
               <hr />
             </li>
             <li>
               <hr />
               <div className="timeline-middle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd" />
-                </svg>
+                <FontAwesomeIcon icon={fa3} />
               </div>
-              <div className="timeline-end timeline-box">iPhone</div>
+              <div className="timeline-end timeline-box">
+                SELECT SLOTS COMPONENT
+              </div>
               <hr />
             </li>
-            <li>
-              <hr />
-              <div className="timeline-middle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd" />
-                </svg>
-              </div>
-              <div className="timeline-end timeline-box">Apple Watch</div>
-            </li>
+
           </ul>
         </div>
       </div >
