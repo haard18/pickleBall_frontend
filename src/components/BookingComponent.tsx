1867f1ff03c210df3bd8ecbfb927a74e2a3665d7
haard18 <haardsolanki.itm@gmail.com>
2024-07-01 23:32:08 +0530

    chore: Refactor BookingComponent and Choose components

--- src/components/BookingComponent.tsx
+++ src/components/BookingComponent.tsx
@@ -1,58 +1,97 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BookingComponent = () => {
-  const [slots, setSlots] = useState({});
-  const [selectedSlots, setSelectedSlots] = useState<{ [key: string]: string[] }>({});
+interface Slot {
+  from: string;
+  to: string;
+  isBooked: boolean;
+}
 
+interface SlotsByDate {
+  [date: string]: Slot[];
+}
+
+interface SelectedSlot {
+  date: string;
+  from: string;
+  to: string;
+}
+
+const BookingComponent: React.FC = () => {
+  const [slots, setSlots] = useState<SlotsByDate>({});
+  const [selectedSlots, setSelectedSlots] = useState<SelectedSlot[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const formatDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed in JavaScript
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
   useEffect(() => {
     const fetchSlots = async () => {
       try {
-        const response = await axios.get('https://pickleball.haardsolanki-itm.workers.dev/api/booking/getSlots/2024-07-01,2024-07-07');
+        const date = new Date();
+        const startDate = new Date(date);
+        const endDate = new Date(startDate);
+        const formattedStartDate = formatDate(startDate);
+        endDate.setDate(startDate.getDate() + 7);
+        const formattedEndDate = formatDate(endDate);
+        const response = await axios.get(`https://pickleball.haardsolanki-itm.workers.dev/api/booking/getSlots/${formattedStartDate},${formattedEndDate}`);
         setSlots(response.data.slots);
       } catch (error) {
         console.error('Error fetching slots:', error);
+      } finally {
+        setIsLoading(false);
       }
     };
-    
+
     fetchSlots();
   }, []);
 
-  const toggleSlotSelection = (day: string, slot: string) => {
-    const daySlots = selectedSlots[day] || [];
-    if (daySlots.includes(slot)) {
-      setSelectedSlots({ ...selectedSlots, [day]: daySlots.filter(s => s !== slot) });
+  const toggleSlotSelection = (date: string, from: string, to: string) => {
+    const selectedSlot = { date, from, to };
+    const index = selectedSlots.findIndex(slot => slot.date === date && slot.from === from && slot.to === to);
+    if (index !== -1) {
+      setSelectedSlots(selectedSlots.filter(slot => slot.date !== date || slot.from !== from || slot.to !== to));
     } else {
-      setSelectedSlots({ ...selectedSlots, [day]: [...daySlots, slot] });
+      setSelectedSlots([...selectedSlots, selectedSlot]);
     }
   };
 
-  const bookSlots = () => {
-    console.log('Booking slots:', selectedSlots);
-    // Implement booking logic here
+  const bookSlots = async () => {
+    //
   };
 
   return (
-    <div className="grid grid-cols-7 gap-4">
-      {Object.entries(slots).map(([date, slotArray]) => (
-        <div key={date} className="border p-4 w">
-          <h2 className="font-bold text-lg">{date}</h2>
-          {slotArray.map(({ from, to, isBooked }: { from: string, to: string, isBooked: boolean }) => (
-            <button
-              key={from}
-              className={`block w-full text-left my-1 p-2 ${isBooked ? 'bg-red-500 text-white' : selectedSlots[date]?.includes(from) ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-              onClick={() => !isBooked && toggleSlotSelection(date, from)}
-              disabled={isBooked}
-            >
-              {`${from.split(':')[0]}:${from.split(':')[1]} - ${to.split(':')[0]}:${to.split(':')[1]}`}
-            </button>
-          ))}
-        </div>
-      ))}
-      <button className="col-span-2 mt-4 bg-green-500 text-white p-2" onClick={bookSlots}>
-        Book Selected Slots
-      </button>
-    </div>
+    <>
+      <div className="grid grid-cols-7 gap-4">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          Object.entries(slots).map(([date, slotArray]) => (
+            <div key={date} className="border p-4 w">
+              <h2 className="font-bold text-lg">{date}</h2>
+              {slotArray.map(({ from, to, isBooked }) => (
+                <button
+                  key={from}
+                  className={`block w-full text-left my-1 p-2 ${isBooked ? 'bg-red-500 text-white' : selectedSlots.find(slot => slot.date === date && slot.from === from && slot.to === to) ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+                  onClick={() => !isBooked && toggleSlotSelection(date, from, to)}
+                  disabled={isBooked}
+                >
+                  {`${from.split(':')[0]}:${from.split(':')[1]} - ${to.split(':')[0]}:${to.split(':')[1]}`}
+                </button>
+              ))}
+            </div>
+          ))
+        )}
+      </div>
+      {!isLoading &&
+        <div className='flex justify-start'>
+          <button className="col-span-2 mt-4 bg-green-500 text-white p-2" onClick={bookSlots}>
+            Book Selected Slots
+          </button>
+        </div>}
+    </>
   );
 };
 
