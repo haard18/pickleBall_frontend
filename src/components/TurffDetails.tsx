06e1a44eb9e5a12ae6d2d11438d6fce503808a5e
haard18 <haard-cse-student@itmbu.ac.in>
2024-07-25 22:45:42 +0530

    refactor: Update TurffDetails component UI for better responsiveness

--- src/components/TurffDetails.tsx
+++ src/components/TurffDetails.tsx
@@ -2,7 +2,7 @@ import Carousal from '../components/Carousal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faShareAlt, faStar } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
-export const TurffDetails = () => {
+export const TurffDetails = ({ title }: { title: string }) => {
     return (
         <>
             <div className='flex w-full justify-center '>
@@ -17,42 +17,43 @@ export const TurffDetails = () => {
 
                     <div className="flex gap-4 justify-center items-center">
                         <div className="address gap-5 flex flex-col items-center" >
-                            <h3 className="text-2xl">Pickle Ball Court</h3>
+                            <h3 className="text-2xl">{title}</h3>
                             <div className='flex gap-2'>
                                 <div className="location-marker ">
                                     <FontAwesomeIcon icon={faMapMarkerAlt} />
                                 </div>
                                 <p className="text-1xl w-76 text ml-2 laptop:w-80">
-                                    Behind Doggers Park, Opposite Veda Lawns, Near Greenwood Antica, Ankodiya Sevasi Road Vadodara, Gujarat 391330
+                                Dhairya Prasad Palace, Opp Lal Baug Under Lal Baug Bridge, Main Gate, beside Patanjali Store, Manjalpur, Vadodara, Gujarat 390011
                                 </p>
                             </div>
                         </div>
                     </div>
                     <div className="right">
-                    <div className="flex flex-row items-center gap-20  mt-10 laptop:flex-col ">
+                        <div className="flex flex-row items-center gap-20  mt-10 laptop:flex-col ">
 
 
-                        <div className="flex flex-row gap-5 laptop:gap-5 ">
-                            <FontAwesomeIcon icon={farHeart} />
-                            <FontAwesomeIcon icon={faShareAlt} />
+                            <div className="flex flex-row gap-5 laptop:gap-5 ">
+                                <FontAwesomeIcon icon={farHeart} />
+                                <FontAwesomeIcon icon={faShareAlt} />
+                            </div>
+
+                            <div className="ratings flex flex-row gap-2  p-1 w-16">
+                                <h1>5.00</h1>
+                                <FontAwesomeIcon icon={faStar} className='mt-1' />
+
+                            </div>
+
                         </div>
-
-                        <div className="ratings flex flex-row gap-2  p-1 w-16">
-                            <h1>5.00</h1>
-                            <FontAwesomeIcon icon={faStar} className='mt-1' />
-
-                        </div>
-
-                    </div>
-                    <div className="getdirections mt-5">
+                        <div className="getdirections mt-5">
                             <div className="flex flex-col">
-
-                                <button className="btn btn-outline">
-                                    Get Directions
-                                </button>
+                                <a target='_blank' href="https://maps.app.goo.gl/7oHshVL2MMWVbqjE8">
+                                    <button className="btn btn-outline" >
+                                        Get Directions
+                                    </button>
+                                </a>
                             </div>
                         </div>
-                        </div>
+                    </div>
                 </div>
 
             </div >
